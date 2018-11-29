c9101816010451a21ee90002a8824b32d6600842
William C Barnes <kit_barnes@yahoo.com>
2018-11-29 08:13:05 -0500

    change from replace buttons to submit buttons
    Signed-off-by: William C Barnes <kit_barnes@yahoo.com>

--- source/HTTPserver.lua
+++ source/HTTPserver.lua
@@ -2,57 +2,53 @@
 
 setlights = function(btn)
 	stop();
-	if btn == "all" then off();
-	elseif btn == "p1" then p1();
-	elseif btn == "p2" then p2();
-	elseif btn == "p3" then p3();
+	if btn == "Off" then off();
+	elseif btn == "Sparkle" then p1();
+	elseif btn == "Waves" then p2();
+	elseif btn == "Pulse" then p3();
 	else return;
 	end
 	pattern = btn;
 end
  
-setlights("all");
-
-local boundary = "not set"	-- INVALID - contains space
+setlights("Sparkle");
 
 srv=net.createServer(net.TCP);
 srv:listen(80,function(conn)
 	conn:on("receive",function(conn,payload)
 		print(payload);
-		if payload:match("^POST /file ") then
-			boundary = payload:match("%sboundary=(%-%-%-%-%S*)");
-			print("boundary set: "..boundary);
-			return	-- nothing sent - won't trigger disconnect
-		end
-		local pagename = pattern;
-		local btn = payload:match("^GET /(%S*)");
-		print(btn);
-		if payload:match(boundary) then
-			local filename = payload:match(' filename="(%S*)"');
-			local filestart = payload:match("\n[\r]*\n()");
-			local fileend = payload:match("()[\r]*\n"..boundary, filestart);
-			if #filename and filestart and fileend then
-				print(filename, filestart, fileend)
-				if file.open(filename,"w") then
-					file.write(payload:sub(filestart,fileend));
-					file.close();
-				end
-			end
-		elseif btn=="upload" or btn=="all" or btn=="p1" or btn=="p2" or btn=="p3" then
-			if pattern ~= btn then setlights(btn) end
-			pagename = btn;
-		elseif btn=="favicon.ico" then
+		if payload:match("^GET /%s") then
+			conn:send ([[
+HTTP/1.0 200 OK
+
+<!DOCTYPE html>
+<html>
+<title>Christmas Tree Lights</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+<h2>Pattern</h2>
+<form method=post action="control" enctype="text/plain">
+  <input type="submit" name="pattern" value="Off">
+  <input type="submit" name="pattern" value="Sparkle">
+  <input type="submit" name="pattern" value="Waves">
+  <input type="submit" name="pattern" value="Pulse">
+</form>
+</body>
+</html>
+]]
+			)
+			print("page sent");
+		elseif payload:match("^POST /control%s") then
+			local pat = payload:match("pattern=(%S*)")
+			if not pat then pat = "nil"; end
+			setlights(pat);
+			conn:send('HTTP/1.0 200 OK');
+			print("OK sent - btn="..pat);
+		else
 			conn:send('HTTP/1.0 404 Not Found');
 			print("404 sent");
-			return
 		end
-		print(pagename);
-		local page = "error?";
-		if file.open(pagename..".htm") then
-			page = file.read();
-			file.close();
-		end
-		conn:send(page);
 	end)
 	conn:on("sent",function(conn)
 		conn:close();
